f24fd618f0b07944c106476baabadbbdab4cb30b
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2018-05-07 14:11:46 -0400

    123

--- JasmitSinghSymposium/src/patient/PatientInfoScreen.java
+++ JasmitSinghSymposium/src/patient/PatientInfoScreen.java
@@ -27,17 +27,17 @@ public class PatientInfoScreen extends AbstractScreen {
 		});
 		back.setEnabled(false);
 		
-		TextArea info = new TextArea(115, 50, 400, 225, Main.getPatient());
-		info.setSize(36);
-		TextArea info1 = new TextArea(115, 106, 400, 225, readLine(1,file));
-		info1.setSize(24);
-		//TextArea info2 = new TextArea(115, 150, 400, 225, readLine(5,file));
+		TextArea name = new TextArea(115, 50, 400, 225, Main.getPatient());
+		name.setSize(36);
+		TextArea dob = new TextArea(115, 106, 400, 225, readLine(1,file));
+		dob.setSize(24);
+		TextArea number = new TextArea(115, 150, 400, 225, readLine(2,file));
 		//TextArea info3 = new TextArea(115, 188, 400, 225, readLine(8,file));
 		
 		viewObjects.add(back);
-		viewObjects.add(info);
-		viewObjects.add(info1);
-		//viewObjects.add(info2);
+		viewObjects.add(name);
+		viewObjects.add(dob);
+		viewObjects.add(number);
 		//viewObjects.add(info3);
 	}
 }
